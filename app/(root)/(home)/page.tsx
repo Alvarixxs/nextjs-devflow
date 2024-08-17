fe63f909e54385a750abf25d0d5d6a6f0b827d83
alvarixxs <alvarodesants@gmail.com>
2024-08-17 15:21:17 -0400

    add auth

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
+const Home = () => {
+  return (
+    <div>
+      <p>hello</p>
+      <UserButton />
+    </div>
+  );
+};
+
+export default Home;
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">GuiGui we are coming</h1>
-    </div>
-  );
-}
