d054eaf6e84015afc0e32170853ffbecb3ef0254
alvarixxs <alvarodesants@gmail.com>
2024-08-19 19:37:41 -0400

    added right sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import Navbar from "@/components/shared/navbar/Navbar";
+import RightSidebar from "@/components/shared/RightSidebar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -13,7 +14,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
 
-        {/* RightSidebar */}
+        <RightSidebar />
       </div>
       {/* Toaster */}
     </main>
